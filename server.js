6b6620fc6b5e6e9d251a5441300655c5357864ae
amgad_youssef <amgad@apple.com>
2017-09-19 14:58:12 -0700

    simplifying the server

--- server.js
+++ server.js
@@ -5,38 +5,26 @@ const express = require('express');
 const webpack = require('webpack');
 const webpackMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
-const config = require('./webpack.config.dev.js');
+const devConfig = require('./webpack.config.dev.js');
 
-const isDevelopment = process.env.NODE_ENV !== 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 const port = process.env.PORT || 4000;
 const app = express();
 
-if (isDevelopment) {
-	const compiler = webpack(config);
-	const middleware = webpackMiddleware(compiler, {
-		publicPath: config.output.publicPath,
-		contentBase: 'app',
-		stats: {
-			colors: true,
-			hash: false,
-			timings: true,
-			chunks: false,
-			chunkModules: false,
-			modules: false
-		}
+if (isProduction) {
+	app.use(express.static(__dirname + '/dist'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, 'dist/index.html'));
 	});
-
+} else {
+	const compiler = webpack(devConfig);
+	const middleware = webpackMiddleware(compiler);
 	app.use(middleware);
 	app.use(webpackHotMiddleware(compiler));
 	app.use(express.static(__dirname));
 	app.get('*', (req, res) => {
 		res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
 		res.end();
-	});
-} else {
-	app.use(express.static(__dirname + '/dist'));
-	app.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname, 'dist/index.html'));
 	});
 }
 
